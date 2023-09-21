4e429b5f64a21c467588ab71aa98adf714bd8653
khaled.dev <salehkhaled.dev@gmail.com>
2023-09-21 15:59:37 +0300

    better remove

--- tasks.js
+++ tasks.js
@@ -146,16 +146,18 @@ function add(text){
  * @returns {void}
  */
 function remove(text){
+  let index=text.slice(6,text.length).trim()-1;
 
   if(text.trim().length==6){
     tasks.pop();
     console.log("Last element Removed succesfully")
   }
-  else{
-    let index=text.slice(6,text.length).trim()-1;
-tasks.splice(index,1);
-console.log(`Task ${index+1} Removed succesfully`)
+  else if(index>=tasks.length || index<0){
+    console.log("Error, number of task does not exist")
+  }
+  tasks.splice(index,1);
+  console.log(`Task ${index+1} Removed succesfully`)
 
-}}
+}
 // The following line starts the application
 startApp("Khaled Saleh")
